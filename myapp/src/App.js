72e490123d84b1b5351c0cda776bb0cb733b6876
Mageta Sukulu <magetasukulu@gmail.com>
2022-09-22 14:27:46 +0300

    Update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,12 @@
-
+import Ninjas from './ninja';
 
 function App() {
   return (
     <div className="App">
       <h1>My first React app!</h1>
       <p>Welcome :)</p>
+      
+      <Ninjas />
     </div>
   );
 }
---
+++ myapp/src/ninja.js
@@ -0,0 +1,11 @@
+function Ninjas() {
+    return (
+      <div className="ninja">
+        <div>Name: Mageta</div>
+        <div>Age: 30</div>
+        <div>Belt: Black</div>
+      </div>
+    );
+  }
+  
+export default Ninjas;
