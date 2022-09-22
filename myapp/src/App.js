d28eb3b7e79f16fd100839bf3f4a3beb1b05220c
Mageta Sukulu <magetasukulu@gmail.com>
2022-09-22 19:20:18 +0300

    Update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,33 @@
 import Ninjas from './ninja';
 import AddNinja from './addNinja';
+import React, { Component } from 'react';
 
-function App() {
-  const state = {
+class App extends Component {
+  state = {
     ninjas : [
       { name: 'Mageta', age: 25, belt: 'black', id: 1 },
       { name: 'Winnie', age: 28, belt: 'yellow', id: 2 },
-      { name: 'Msuya', age: 20, belt: 'white', id: 3 }
+      { name: 'Msuya', age: 20, belt: 'white', id: 3 },
     ]
   }
-  return (
-    <div className="App">
-      <h1>My first React app!</h1>
-      <p>Welcome :)</p>
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app!</h1>
+        <p>Welcome :)</p>
 
-      <Ninjas ninjas={ state.ninjas} />
-      <AddNinja />
-    </div>
-  );
+        <Ninjas ninjas={ this.state.ninjas} />
+        <AddNinja addNinja={this.addNinja} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- myapp/src/addNinja.js
+++ myapp/src/addNinja.js
@@ -1,9 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
+
 class AddNinja extends Component {
     state = {
         name: null,
         age: null,
-        belt: null
+        belt: null,
     }
     handleChange = (e) => {
         this.setState({
@@ -12,7 +13,7 @@ class AddNinja extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state)
+        this.props.addNinja(this.state);
     }
 
     render() {
@@ -21,9 +22,9 @@ class AddNinja extends Component {
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="name">Name:</label>
                     <input type="text" id="name" onChange={this.handleChange} />
-                    <label htmlFor="name">Age:</label>
+                    <label htmlFor="age">Age:</label>
                     <input type="text" id="age" onChange={this.handleChange} />
-                    <label htmlFor="name">Belt:</label>
+                    <label htmlFor="belt">Belt:</label>
                     <input type="text" id="belt" onChange={this.handleChange} />
                     <button>Submit</button>
                 </form>
